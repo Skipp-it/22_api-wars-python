7cea8fbb71e6edce385e6a74b978aa64688a0700
Celalalt666 <mihairaducroitoru@gmail.com>
2020-07-05 17:43:04 +0300

    production

--- database_common.py
+++ database_common.py
@@ -1,10 +1,12 @@
 # Creates a decorator to handle the database connection/cursor opening/closing.
 # Creates the cursor with RealDictCursor, thus it returns real dictionaries, where the column names are the keys.
-import os
-
-import psycopg2
 import psycopg2.extras
+
+import os
+import psycopg2
 import urllib
+
+production = True
 
 
 
@@ -34,8 +36,19 @@ def get_connection_string():
 
 def open_database():
     try:
-        connection_string = get_connection_string()
-        connection = psycopg2.connect(connection_string)
+        if not production:
+            connection_string = get_connection_string()
+            connection = psycopg2.connect(connection_string)
+        else:
+            urllib.parse.uses_netloc.append('postgres')
+            url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+            connection = psycopg2.connect(
+                database=url.path[1:],
+                user=url.username,
+                password=url.password,
+                host=url.hostname,
+                port=url.port
+            )
         connection.autocommit = True
     except psycopg2.DatabaseError as exception:
         print('Database connection problem')
