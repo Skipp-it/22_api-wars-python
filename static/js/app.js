344dc0dc48440a1637a61cbd451c3c37d8d67fc5
Skipp-it <mihairaducroitoru@gmail.com>
2020-07-08 16:08:24 +0300

    fetch problem

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,3 @@
-
 let planetsTBody = document.querySelector('.planets')
 let nextBtn = document.getElementById('next')
 let previousBtn =document.getElementById('previous')
@@ -9,7 +8,6 @@ const closeModalBtn = document.querySelector('.modal__actions')
 const planetsTable = document.querySelector('.planets-table')
 const votesTable = document.querySelector('.votes-table')
 const closeModalVote = document.querySelector('#close-vote')
-
 
 
 closeModalVote.addEventListener('click', ()=>{
@@ -30,16 +28,20 @@ async function getPlanets(planetsEndpoint) {
     let data = await fetch(`${planetsEndpoint}`)
     let jsonData = await data.json()
     let resultData = jsonData.results
-    nextBtn.setAttribute('data-fetch', jsonData.next)
-    if (jsonData.next === null) {
+    let nextUrl = jsonData.next
+    let previousUrl = jsonData.previous
+    if (nextUrl === null) {
         nextBtn.setAttribute('disabled', true)
     } else {
+        let nextHttps = nextUrl.replace("http", 'https')
+        nextBtn.setAttribute('data-fetch', nextHttps)
         nextBtn.removeAttribute('disabled')
     }
-    previousBtn.setAttribute('data-fetch', jsonData.previous)
-    if (jsonData.previous === null) {
+    if (previousUrl === null) {
         previousBtn.setAttribute('disabled', true)
     } else {
+        let prevHttps = previousUrl.replace("http", 'https')
+        previousBtn.setAttribute('data-fetch', prevHttps)
         previousBtn.removeAttribute('disabled')
     }
 
@@ -56,7 +58,9 @@ async function getPlanets(planetsEndpoint) {
         tdElementName.innerHTML = `<td>${resultData[i].name}</td>`
         list[i].appendChild(tdElementName)
         let tdElementDiam = document.createElement('td')
-        tdElementDiam.innerHTML = `<td> ${numberWithCommas(resultData[i].diameter)} km </td>`
+        if (`${resultData[i].diameter}` === '0') {
+            tdElementDiam.innerHTML = `<td> unknown </td>`
+        } else {tdElementDiam.innerHTML = `<td> ${numberWithCommas(resultData[i].diameter)} km </td>`}
         list[i].appendChild(tdElementDiam)
         let tdElementClimate = document.createElement('td')
         tdElementClimate.innerHTML = `<td> ${resultData[i].climate} </td>`
@@ -65,10 +69,12 @@ async function getPlanets(planetsEndpoint) {
         tdElementTerrain.innerHTML = `<td> ${resultData[i].terrain}</td>`
         list[i].appendChild(tdElementTerrain)
         let tdElementSWater = document.createElement('td')
-        tdElementSWater.innerHTML = `<td> ${resultData[i].surface_water}</td>`
+        tdElementSWater.innerHTML = `<td> ${resultData[i].surface_water} %</td>`
         list[i].appendChild(tdElementSWater)
         let tdElementPopulation = document.createElement('td')
-        tdElementPopulation.innerHTML = `<td> ${numberWithCommas(resultData[i].population)} population</td>`
+        if (`${resultData[i].population}`=== 'unknown') {
+            tdElementPopulation.innerHTML = `<td> unknown </td>`
+        } else { tdElementPopulation.innerHTML = `<td> ${numberWithCommas(resultData[i].population)} inhabitants</td>` }
         list[i].appendChild(tdElementPopulation)
         let residents = document.createElement('td')
         let residentsListLength = resultData[i].residents.length
@@ -116,7 +122,14 @@ previousBtn.addEventListener('click', ()=>{
     }
 })
 
+function toggleLoadingScreen() {
+    document.querySelector('.loader-wrapper').classList.toggle('visible')
+    document.querySelector('.loader-inner').classList.toggle('visible')
+    document.querySelector('.loader').classList.toggle('visible')
+}
+
 function openModal() {
+    toggleLoadingScreen()
     const divPlanet = document.querySelector('.planet')
     const planetName = this.getAttribute('data-planet')
     divPlanet.innerHTML = `<h6>Residents of ${planetName}</h6>`
@@ -129,29 +142,35 @@ function openModal() {
     const accessResident = () => {
         residents.forEach(async resident => {
             let trModalData = document.createElement('tr')
-            const residentDetails = await fetch(`${resident}`)
+            let httpsResident = resident.replace('http', 'https')
+            const residentDetails = await fetch(`${httpsResident}`)
             const residentJson = await residentDetails.json()
 
             trModalData.innerHTML = `       <td>${residentJson.name}</td>
-                                            <td>${residentJson.height}</td>
-                                            <td>${residentJson.mass}</td>
+                                            <td>${residentJson.height} cm</td>
+                                            <td>${residentJson.mass} kg</td>
                                             <td>${residentJson.hair_color}</td>
                                             <td>${residentJson.skin_color}</td>
                                             <td>${residentJson.eye_color}</td>
                                             <td>${residentJson.birth_year}</td>
                                             <td>${residentJson.gender}</td>`
             tableModalData.appendChild(trModalData)
-
+            let nrTr = tableModalData.querySelectorAll('tr')
+            if (nrTr.length === residents.length) {
+                toggleLoadingScreen()
+            }
         })
     }
     accessResident()
 }
+
 
 function voteModal() {
     const divPlanet = document.querySelector('.votes')
     const planetName = this.getAttribute('data-planet')
     const planetID = this.getAttribute('data-index')
     saveDoc(planetID, planetName)
+    //
     let tableModalDataVotes = document.querySelector('.modal_table_votes')
     tableModalDataVotes.innerHTML = ""
     backdrop.classList.toggle('visible')
@@ -160,40 +179,69 @@ function voteModal() {
     divPlanet.innerHTML = `<h6>Residents of ${planetName}</h6>`
 }
 
+// //GET
+// function loadDoc(planetID, planetName) {
+//       let xhttp = new XMLHttpRequest();
+//       let tableModalDataVotes = document.querySelector('.modal_table_votes')
+//       xhttp.onreadystatechange = function() {
+//           if (this.readyState == 4 && this.status == 200) {
+//               const data = JSON.parse(xhttp.responseText)
+//               data.forEach(elem=>{
+//                     let trModalVotes = document.createElement('tr')
+//                     trModalVotes.innerHTML = `
+//                                                     <td>${elem.planet}</td>
+//                                                     <td>${elem.count}</td>`
+//                     tableModalDataVotes.appendChild(trModalVotes)
+//               })
+//   }};
+//   xhttp.open("GET", "/votes", true);
+//   xhttp.send(); // eroare in Heroku
+// }
 
-function loadDoc(planetID, planetName) {
-      let xhttp = new XMLHttpRequest();
-      let tableModalDataVotes = document.querySelector('.modal_table_votes')
-      xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-                const data = JSON.parse(xhttp.responseText)
-                data.forEach(elem=>{
-                    let trModalVotes = document.createElement('tr')
-                    trModalVotes.innerHTML = `      
-                                                    <td>${elem.planet}</td>
-                                                    <td>${elem.count}</td>`
-                    tableModalDataVotes.appendChild(trModalVotes)
-                })
-  }};
-
-  xhttp.open("GET", "/vote/" + planetID + "/" + planetName, true);
-  xhttp.send();
-}
+// //POST
+// function saveDoc(planetID, planetName) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.onload = function () {
+//         loadDoc(planetID, planetName)
+//     }
+//     xhr.open("POST", "/vote/" + planetID + "/" + planetName, true);
+//     //Send the proper header information along with the request
+//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//     xhr.send(`id=${planetID}&name=${planetName}`);
+// }
+//
+//
 
 
-function saveDoc(planetID, planetName) {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-        loadDoc(planetID, planetName)
-    }
-    xhr.open("POST", "/vote/" + planetID + "/" + planetName, true);
-    //Send the proper header information along with the request
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send(`id=${planetID}&name=${planetName}`);
+// //GET
+// async function loadDoc(planetID, planetName) {
+//     let data = await fetch(`/votes`)
+//     let jsonData = await data.json()
+//     console.log(jsonData)
+//     return jsonData
+// }
+
+//POST
+async function saveDoc(planetID, planetName) {
+    let submitData = {
+        'planet_id': `${planetID}`,
+        'planet_name': `${planetName}`
+    };
+    let response = await fetch(`/vote`, {
+        method: "POST",
+        mode: "cors",
+        cache: "default",
+        credentials: "include",
+        headers: {"Content-Type": "application/json"},
+        redirect: "follow",
+        body: JSON.stringify(submitData)
+    })
+    let result = await response.json()
+    // console.log(response)
+    console.log(result.body)
 }
 
 
 getPlanets(variable)
-
 
 
