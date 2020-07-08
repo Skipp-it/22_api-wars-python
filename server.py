9610f554967f840853059ef6e80b56cf75ebb852
Skipp-it <mihairaducroitoru@gmail.com>
2020-07-08 16:08:43 +0300

    fetch problem

--- server.py
+++ server.py
@@ -1,18 +1,16 @@
 from datetime import datetime
-from flask import Flask, render_template, url_for, request, session, redirect, flash
+from flask import Flask, render_template, url_for, request, session, redirect, flash, jsonify, make_response
 import data_manager
 from util import json_response
 
 
 app = Flask('__name__')
-# app.secret_key = data_manager.random_api_key()
-app.secret_key = '123'
+app.secret_key = data_manager.random_api_key()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -59,20 +57,41 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route("/vote/<planetID>/<planetName>", methods=['GET', 'POST'])
+# @app.route("/vote/<planetID>/<planetName>", methods=['GET', 'POST'])
+# @json_response
+# def vote(planetID, planetName):
+#     # if request.method == 'POST':
+#         planet_id = planetID
+#         planet_name = planetName
+#         user_id = session['user_id']
+#         submission_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#         data_manager.vote_planet(planet_id, planet_name, user_id, submission_time)
+#         flash(f'Voted on planet {planetName} successfully')
+#         return "ok", 200
+
+@app.route("/vote", methods=['GET', 'POST'])
 @json_response
-def vote(planetID, planetName):
-    if request.method == 'POST':
-        planet_id = planetID
-        planet_name = planetName
-        user_id = session['user_id']
-        submission_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        data_manager.vote_planet(planet_id, planet_name, user_id, submission_time)
-        flash(f'Voted on planet {planetName} successfully')
-        return render_template('index.html')
+def vote():
+    data = request.get_json()
+    planet_id = data['planet_id']
+    planet_name = data['planet_name']
     user_id = session['user_id']
-    date = data_manager.planets_votes(user_id)
-    return date
+    submission_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    data_manager.vote_planet(planet_id, planet_name, user_id, submission_time)
+    # flash(f'Voted on planet {planet_name} successfully')
+    updated_data = data_manager.planets_votes(user_id)
+    response = make_response(jsonify({"message": "JSON received", 'body': updated_data}), 200)
+    print(response)
+    return response
+
+
+#
+# @app.route("/votes")
+# @json_response
+# def votes():
+#     user_id = session['user_id']
+#     date = data_manager.planets_votes(user_id)
+#     return date
 
 
 def main():
