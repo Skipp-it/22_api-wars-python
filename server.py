368222735846814c0b2fbc650d9d4dd0cc1a7816
Celalalt666 <mihairaducroitoru@gmail.com>
2020-07-09 15:48:41 +0300

    fetch solved final commit

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, request, session, redirect, flash, jsonify, make_response
 import data_manager
-from util import json_response
+
 
 
 app = Flask('__name__')
