a3e66b169b1b66d1b3bb988cce566c8863be1def
Celalalt666 <mihairaducroitoru@gmail.com>
2020-07-09 16:23:19 +0300

    final commit

--- database_common.py
+++ database_common.py
@@ -41,6 +41,7 @@ def open_database():
             urllib.parse.uses_netloc.append('postgres')
             url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
             connection = psycopg2.connect(
+                sslmode="require",
                 database=url.path[1:],
                 user=url.username,
                 password=url.password,
